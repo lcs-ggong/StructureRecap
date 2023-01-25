b755a824ae9619c410f47db6d2c875cf1ed9c8f9
Grace Gong <ggong@lcs.on.ca>
2023-01-25 11:19:09 -0500

    Initial Commit

---
+++ StructureRecap/StructureRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructureRecapApp.swift
+//  StructureRecap
+//
+//  Created by Grace Gong on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct StructureRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
